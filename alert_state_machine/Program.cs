6a6a45984dbe0f08719c42b2263debb46991abd3
n0bis <madsfalken@Madss-MacBook-Pro.local>
2019-10-24 21:25:31 +0200

    Update Program.cs

--- alert_state_machine/Program.cs
+++ alert_state_machine/Program.cs
@@ -19,16 +19,6 @@ namespace alert_state_machine
             var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
             var config = builder.Build();
 
-
-            /*var opts = new PureWebSocketOptions { MyReconnectStrategy = new ReconnectStrategy(500, 2000), DebugMode = true };
-            var weather = new PureWebSocket("wss://swd.weatherflow.com/swd/data?api_key=a8f5dbda-af0a-4b57-99b9-f10baa88f27b", opts);
-            weather.OnMessage += (sender, message) => Console.WriteLine($"{sender} : {message}", ConsoleColor.Green);
-            await weather.ConnectAsync();
-            await weather.SendAsync("{\"type\":\"listen_start\", \"device_id\":5356}");
-            await weather.SendAsync("{\"type\":\"listen_rapid_start\", \"device_id\":5356}");
-            await weather.SendAsync("{\"type\":\"listen_start\", \"device_id\":12202}");
-            await weather.SendAsync("{\"type\":\"listen_rapid_start\", \"device_id\":12202}");*/
-
             /*var utmService = new UTMService(config);
             await utmService.Auth();
 
