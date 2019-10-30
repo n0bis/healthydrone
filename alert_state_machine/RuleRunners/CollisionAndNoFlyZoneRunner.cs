01a803ffb1e81b106aee7282d29d241eb60a7921
n0bis <madsfalken@Madss-MacBook-Pro.local>
2019-10-30 13:31:39 +0100

    refactor weatherservice and start nocollision

---
+++ alert_state_machine/RuleRunners/CollisionAndNoFlyZoneRunner.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace alert_state_machine.RuleRunners
+{
+    public class CollisionAndNoFlyZoneRunner
+    {
+        public CollisionAndNoFlyZoneRunner()
+        {
+        }
+    }
+}
--- alert_state_machine/RuleRunners/WeatherRunner.cs
+++ alert_state_machine/RuleRunners/WeatherRunner.cs
@@ -13,43 +13,38 @@ namespace alert_state_machine.RuleRunners
 {
     public static class WeatherRunner
     {
-        public static async Task WeatherCheck(IConfiguration config)
+        public static async Task WeatherCheck(IConfiguration config, UTMService utmService)
         {
-            using (var utmService = new UTMService(config["UTM:clientid"], config["UTM:clientsecret"],
-                config["UTM:username"], config["UTM:password"]))
+            var weatherService = new WeatherService();
+            var redisService = new RedisService(config);
+            List<Flight> flights = await utmService.Operation.GetFlightsInAllOperationsAsync();
+            flights?.ForEach(async flight =>
             {
-                var weatherService = new WeatherService();
-                var redisService = new RedisService(config);
-                List<Flight> flights = await utmService.Operation.GetFlightsInAllOperationsAsync();
-                flights?.ForEach(async flight =>
-                {
-                    var flightCoordinates = flight.coordinate;
-                    var weatherResponse = await weatherService.GetWeatherAtCoord(latitude: flightCoordinates.latitude.ToString(), longitude: flightCoordinates.longitude.ToString());
-                    redisService.Connect();
-                    var process = new Process();
-                    var key = $"{flight.uasOperation}-{flight.uas.uniqueIdentifier}-weather";
-                    var cachedProcess = await redisService.Get<State>(key);
-                    if (cachedProcess != null) {
-                        process.CurrentState = cachedProcess.CurrentState;
-                    } else {
-                        cachedProcess = new State();
-                    }
+                var flightCoordinates = flight.coordinate;
+                var weatherResponse = await weatherService.GetWeatherAtCoord(latitude: flightCoordinates.latitude.ToString(), longitude: flightCoordinates.longitude.ToString());
+                redisService.Connect();
+                var process = new Process();
+                var key = $"{flight.uasOperation}-{flight.uas.uniqueIdentifier}-weather";
+                var cachedProcess = await redisService.Get<State>(key);
+                if (cachedProcess != null) {
+                    process.CurrentState = cachedProcess.CurrentState;
+                } else {
+                    cachedProcess = new State();
+                }
 
-                    if (weatherResponse.main.temp < 20 && (process.CurrentState == ProcessState.Active || process.CurrentState == ProcessState.Inactive)) {
-                        process.MoveNext();
-                    } else {
-                        process.MovePrev();
-                    }
-
-                    
-                    if (process.CurrentState == ProcessState.Raised && !cachedProcess.Triggered && !cachedProcess.Handled) {
-                        Console.WriteLine($"SEND ALERT FOR: {flight.uasOperation}-{flight.uas.uniqueIdentifier}-weather");
-                        cachedProcess.Triggered = true;
-                    }
-                    cachedProcess.CurrentState = process.CurrentState;
-                    await redisService.Set(key, cachedProcess);
-                });
-            }
+                if (weatherResponse.main.temp < 20 && (process.CurrentState == ProcessState.Active || process.CurrentState == ProcessState.Inactive)) {
+                    process.MoveNext();
+                } else {
+                    process.MovePrev();
+                }
+                
+                if (process.CurrentState == ProcessState.Raised && !cachedProcess.Triggered && !cachedProcess.Handled) {
+                    Console.WriteLine($"SEND ALERT FOR: {flight.uasOperation}-{flight.uas.uniqueIdentifier}-weather");
+                    cachedProcess.Triggered = true;
+                }
+                cachedProcess.CurrentState = process.CurrentState;
+                await redisService.Set(key, cachedProcess);
+            });
         }
     }
 }
