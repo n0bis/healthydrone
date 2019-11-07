bd5fa73ee933ff6e10a3da510e38249ff02c5b4e
n0bis <madsfalken@Madss-MacBook-Pro.local>
2019-11-07 16:26:19 +0100

    create docker container and keep it alive
    docker container can be kept alive with withTty(true)

--- DroneManager.API/src/API/Application.java
+++ DroneManager.API/src/API/Application.java
@@ -1,14 +1,8 @@
 package API;
 
-import UTMService.UTMService;
-import com.spotify.docker.client.DefaultDockerClient;
-import com.spotify.docker.client.DockerClient;
-import com.spotify.docker.client.LogStream;
-import com.spotify.docker.client.exceptions.DockerCertificateException;
-import com.spotify.docker.client.exceptions.DockerException;
-import com.spotify.docker.client.messages.ContainerConfig;
-import com.spotify.docker.client.messages.ExecCreation;
-import com.spotify.docker.client.messages.HostConfig;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.DockerClientConfig;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,32 +13,21 @@ import java.io.IOException;
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) throws DockerCertificateException, DockerException, InterruptedException, IOException {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(Application.class, args);
         //var utmService = new UTMService();
         //utmService.clientTracking().updateFlight(55.355848, 10.440672);
 
-        var docker = DefaultDockerClient.fromEnv().build();
+        DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder().build();
+        var dockerClient = DockerClientBuilder.getInstance(config).build();
 
-        docker.pull("jonasvautherin/px4-gazebo-headless:v1.9.2");
+        var container = dockerClient
+                .createContainerCmd("jonasvautherin/px4-gazebo-headless:v1.9.2")
+                .withEnv("PX4_HOME_LAT=55.3686619", "PX4_HOME_LON=10.4300876", "PX4_HOME_ALT=10")
+                .withTty(true)
+                .exec();
 
-        var hostConfig = HostConfig.builder().build();
-
-        var containerConfig = ContainerConfig.builder()
-                .hostConfig(hostConfig)
-                .image("jonasvautherin/px4-gazebo-headless:v1.9.2")
-                .env("PX4_HOME_LAT=55.3686619", "PX4_HOME_LON=10.4300876", "PX4_HOME_ALT=10")
-                .build();
-
-        var creation = docker.createContainer(containerConfig);
-        String id = creation.id();
-
-        var info = docker.inspectContainer(id);
-        System.out.println(info);
-        docker.startContainer(id);
-
-        var state = docker.inspectContainer(id).state();
-        System.out.println(state);
+        dockerClient.startContainerCmd(container.getId()).exec();
 
         System.in.read();
     }
