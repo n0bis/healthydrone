7b14be6f8b80914a5390a631b5c422cdb41d3914
Mathiask1 <birkebjerg99@live.dk>
2019-11-19 15:52:08 +0100

    Update CollisionAndNoFlyZoneRunner.cs

--- alert_state_machine/RuleRunners/CollisionAndNoFlyZoneRunner.cs
+++ alert_state_machine/RuleRunners/CollisionAndNoFlyZoneRunner.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using alert_state_machine.Persistence;
 using alert_state_machine.Services;
 using utm_service;
 using utm_service.Models;
@@ -8,14 +9,25 @@ namespace alert_state_machine.RuleRunners
 {
     public class CollisionAndNoFlyZoneRunner : ICollisionAndNoFlyZoneRunner
     {
-        public CollisionAndNoFlyZoneRunner()
+        private readonly IRedisService _redisService;
+        private Boolean isConnected = false;
+
+        public CollisionAndNoFlyZoneRunner(IRedisService redisService)
         {
+            _redisService = redisService;
+            _redisService.Connect();
+
         }
 
         public async Task ZonesCheck(Token token)
         {
-            var utmLiveService = new UTMLiveService();
-            await utmLiveService.Connect(token?.access_token);
+            if (isConnected == false)
+            {
+                var utmLiveService = new UTMLiveService();
+                await utmLiveService.Connect(token?.access_token);
+                isConnected = true;
+            }
+
         }
     }
 }
