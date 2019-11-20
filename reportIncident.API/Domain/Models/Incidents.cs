e9d09b12fa0f48f80675a0453b9b0c68ea08032a
Jones_lund <42931029+Coachmeister@users.noreply.github.com>
2019-11-20 20:00:06 +0100

    Som mo

---
+++ reportIncident.API/Domain/Models/Incidents.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace reportIncident.API.Domain.Models
+{
+    public class Incidents
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Operation { get; set; }
+        public string Drone { get; set; }
+        public string Details { get; set; }
+        public string Damage { get; set; }
+        public string Actions { get; set; }
+        public string Notes { get; set; }
+        public FileStyleUriParser File { get; set; }
+        
+
+    }
+}
