fad1129ae5edb13910eaa856200602b3d54644f9
Jones_lund <42931029+Coachmeister@users.noreply.github.com>
2019-11-21 11:32:43 +0100

    ME

---
+++ reportIncident.API/Persistence/Contexts/appDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using reportIncident.API.Domain.Models;
+
+namespace reportIncident.API.Persistence.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Incidents> Incidents { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void onModelCreating(ModelBuilder builder)
+        {
+    
+        }
+    }
+
+}   
