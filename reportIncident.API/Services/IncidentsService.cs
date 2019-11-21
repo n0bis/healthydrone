d963e6799a5ceba97b607ce32f23293f0ed6ca25
Jones_lund <42931029+Coachmeister@users.noreply.github.com>
2019-11-21 11:13:57 +0100

    TOLD

---
+++ reportIncident.API/Services/IncidentsService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using reportIncident.API.Domain.Models;
+using reportIncident.API.Domain.Services;
+
+namespace reportIncident.API.Services
+{
+    public class IncidentsService
+    {
+    }
+}
