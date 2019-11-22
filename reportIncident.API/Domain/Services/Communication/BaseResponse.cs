3e0437c7a17f6e8c3fa25f6b550b2a964b8ebc8e
Jones_lund <jonas.lund@hotmail.dk>
2019-11-22 10:59:49 +0100

    the lion sleeps tonight

---
+++ reportIncident.API/Domain/Services/Communication/BaseResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using reportIncident.API.Domain.Services.Communication;
+
+namespace reportIncident.API.Domain.Services.Communication
+{
+    public abstract class BaseResponse
+    {
+
+    }
+}
