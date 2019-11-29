197332609b8645067227d2d0579824cbc78d01b1
Mathiask1 <birkebjerg99@live.dk>
2019-11-29 13:03:38 +0100

    start of redis

---
+++ HandleAlerts.API/HandleAlerts.API/Settings/RedisOpts.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace alert_state_machine.Settings
+{
+    public class RedisOpts
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+}
