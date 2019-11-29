b4e2f016e1a63a7557d821197308d5a3ee735537
Mathiask1 <birkebjerg99@live.dk>
2019-11-29 13:41:56 +0100

    dake dak christmas music

--- HandleAlerts.API/HandleAlerts.API/Controllers/AlertsController.cs
+++ HandleAlerts.API/HandleAlerts.API/Controllers/AlertsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using HandleAlerts.API.Domain.Models;
+using HandleAlerts.API.Persistence;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -12,10 +13,19 @@ namespace HandleAlerts.API.Controllers
     [Route("api/[controller]")]
     public class AlertsController : Controller
     {
-        [HttpPost]
-        public Alert Post(Guid uasOperation, Guid uniqueIdentifier, string alertType )
-        {
+        IRedisService _redisService;
 
+        public AlertsController(IRedisService redisService)
+        {
+            _redisService = redisService;
+            _redisService.Connect();
+        }
+
+        [HttpPost]
+        public Alert Post(Guid uasOperation, Guid droneID, string alertType )
+        {
+            var key = $"{uasOperation}-{droneID}-{alertType}";
+            _redisService.Set(key, alertType);
         }
     }
 
--- HandleAlerts.API/HandleAlerts.API/Domain/Models/State.cs
+++ HandleAlerts.API/HandleAlerts.API/Domain/Models/State.cs
@@ -1,18 +1,15 @@
 ﻿using System;
-using alert_state_machine.States;
 
 namespace alert_state_machine.Models
 {
     public class State
     {
-        public ProcessState CurrentState { get; set; }
         private DateTime TimeStamp { get; set; }
         public bool Triggered { get; set; }
         public bool Handled { get; set; }
 
         public State()
         {
-            this.CurrentState = ProcessState.Inactive;
             this.TimeStamp = DateTime.Now;
             this.Triggered = false;
             this.Handled = false;
