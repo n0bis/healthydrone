6ce09f0cb637617bb32c2d7f3b8c67df2462ac00
Frederik Helth <frederikhelth@Frederiks-MacBook-Pro-2.local>
2019-12-09 08:38:13 +0100

    Accepting and making axios request

--- frontend/src/components/Layout/Report.js
+++ frontend/src/components/Layout/Report.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
@@ -9,95 +9,99 @@ import DialogTitle from "@material-ui/core/DialogTitle";
 import { observer, inject } from "mobx-react";
 import { Select } from "antd";
 
-
-
-
 const { Option } = Select;
 
 function handleChange(value) {
   console.log(`selected ${value}`);
 }
 
-export var OpenModal = inject("reportStore")(observer((props) => {
- const {onOpenModal} = props.reportStore;
-  return <p onClick={onOpenModal}> REPORT </p>
- }));
+export var OpenModal = inject("reportStore")(
+  observer(props => {
+    const { onOpenModal } = props.reportStore;
+    return <p onClick={onOpenModal}> REPORT </p>;
+  })
+);
 
 @inject("reportStore")
-@observer 
+@observer
 class FormDialog extends Component {
-
-  render(){
-    const {isModalOpen, onCloseModal, onSendModal} = this.props.reportStore;
+  render() {
+    const {
+      isModalOpen,
+      onCloseModal,
+      onSendModal,
+      handleChange
+    } = this.props.reportStore;
     console.log(this.props.reportStore);
-  return (
-    <Dialog
-      open={isModalOpen}
-      aria-labelledby="form-dialog-title"
-    >
-      <DialogTitle id="form-dialog-title">Rapporter fejl</DialogTitle>
-      <DialogContent>
-        <DialogContentText>
-          Beskriv venligst den fejl der opstod. Fejlen vil herefter blive
-          gennemgået.
-        </DialogContentText>
-        <div>
-    <Select defaultValue="choose a Drone" style={{ width: 120 }} onChange={handleChange}>
-      <Option value="drone1">drone1</Option>
-      <Option value="drone2">drone2</Option>
-      <Option value="drone3">drone3</Option>
-      <Option value="luaaaacy">Lusssscy</Option>
-      <Option value="luaaaqqqcy">Luaaacy</Option>
-      <Option value="lubbbbcy">Luvvsdfcy</Option>
-     
-    </Select>
-  </div>
-        <TextField
-          id="standard-multiline-static"
-          label="Detaljer"
-          placeholder="Fortæl hvad der skete"
-          multiline
-          rowsMax="10"
-          fullWidth
-        />
-        <TextField
-          id="standard-multiline-static"
-          label="Skade"
-          placeholder="Beskriv skaden"
-          multiline
-          rowsMax="10"
-          fullWidth
-        />
-        <TextField
-          id="standard-multiline-static"
-          label="Handling"
-          placeholder="Handling"
-          multiline
-          rowsMax="10"
-          fullWidth
-        />
-        <TextField
-          id="standard-multiline-static"
-          label="Noter"
-          placeholder="Noter"
-          multiline
-          rowsMax="10"
-          fullWidth
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button  color="primary" onClick={onCloseModal}>
-          Anullere
-        </Button>
-        <Button  color="primary" onClick={onSendModal}>
-          Send
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
-  
+    return (
+      <Dialog open={isModalOpen} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Rapporter fejl</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Beskriv venligst den fejl der opstod. Fejlen vil herefter blive
+            gennemgået.
+          </DialogContentText>
+          <Select
+            defaultValue="choose a Drone"
+            style={{ width: 120 }}
+            onChange={handleChange}
+          >
+            <Option value="drone1">drone1</Option>
+            <Option value="drone2">drone2</Option>
+            <Option value="drone3">drone3</Option>
+            <Option value="luaaaacy">Lusssscy</Option>
+            <Option value="luaaaqqqcy">Luaaacy</Option>
+            <Option value="lubbbbcy">Luvvsdfcy</Option>
+          </Select>
+          <TextField
+            id="standard-multiline-static"
+            label="Detaljer"
+            placeholder="Fortæl hvad der skete"
+            multiline
+            rowsMax="10"
+            name="details"
+            onChange={handleChange}
+            fullWidth
+          />
+          <TextField
+            id="standard-multiline-static"
+            label="Skade"
+            placeholder="Beskriv skaden"
+            multiline
+            rowsMax="10"
+            onChange={handleChange}
+            fullWidth
+          />
+          <TextField
+            id="standard-multiline-static"
+            label="Handling"
+            placeholder="Handling"
+            multiline
+            rowsMax="10"
+            fullWidth
+            onChange={handleChange}
+          />
+          <TextField
+            id="standard-multiline-static"
+            label="Noter"
+            placeholder="Noter"
+            multiline
+            rowsMax="10"
+            onChange={handleChange}
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button color="primary" onClick={onCloseModal}>
+            Anullere
+          </Button>
+          <Button color="primary" onClick={onSendModal}>
+            Send
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
 }
-}
- 
 
 export default FormDialog;
--- frontend/src/stores/ReportStore.js
+++ frontend/src/stores/ReportStore.js
@@ -2,20 +2,31 @@ import { get, action, observable, toJS } from "mobx";
 import axios from "axios";
 
 class ReportStore {
-    @observable isModalOpen = false;
+  @observable isModalOpen = false;
+  @observable data = {};
 
-    @action onOpenModal = () => { 
-        this.isModalOpen = true;
-    };
-    @action onCloseModal = () => { 
-        this.isModalOpen = false;
-    };
-    @action onSendModal = e => {
-      console.log(e.target.value)
-     
-        this.isModalOpen = false;
-    }
-   
+  @action onOpenModal = () => {
+    this.isModalOpen = true;
+  };
+  @action onCloseModal = () => {
+    this.isModalOpen = false;
+  };
+
+  @action handleChange = e => {
+    this.data[e.target.name] = e.target.value;
+  };
+
+  @action onSendModal = e => {
+    axios
+      .post("/INSERT_BACK_END_URL", this.data)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    this.isModalOpen = false;
+  };
 }
 
 const reportStore = new ReportStore();
