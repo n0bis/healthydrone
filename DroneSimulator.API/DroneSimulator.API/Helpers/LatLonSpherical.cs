164f13117d50fe1711fa7a3ee518be981fd15925
Jones_lund <jonas.lund@hotmail.dk>
2019-12-11 11:33:32 +0100

    unit test done

--- DroneSimulator.API/DroneSimulator.API/Helpers/LatLonSpherical.cs
+++ DroneSimulator.API/DroneSimulator.API/Helpers/LatLonSpherical.cs
@@ -17,7 +17,14 @@ namespace DroneSimulator.API.Helpers
             return (radian * 180 / Math.PI);
         }
 
-        // Calculate the (initial) bearing between two points, in degrees
+        /// <summary>
+        /// Calculate the (initial) bearing between two points, in degrees
+        /// formula is for the initial bearing (sometimes referred to as forward azimuth):
+        /// θ = atan2( sin Δλ ⋅ cos φ2 , cos φ1 ⋅ sin φ2 − sin φ1 ⋅ cos φ2 ⋅ cos Δλ )
+        /// where: φ1,λ1 is the start point, φ2,λ2 the end point (Δλ is the difference in longitude)
+        /// </summary>
+        /// <returns>The initial bearing from ‘this’ point to destination point.</returns>
+        /// <see cref="http://www.movable-type.co.uk/scripts/latlong.html"/>
         public static double CalculateBearing(Location startPoint, Location endPoint)
         {
             double lat1 = DegreeToRadian(startPoint.latitude);
@@ -32,7 +39,18 @@ namespace DroneSimulator.API.Helpers
             return (RadianToDegree(bearing) + 360) % 360;
         }
 
-        // Calculate the destination point from given point having travelled the given distance (in km), on the given initial bearing (bearing may vary before destination is reached)
+        /// <summary>
+        /// Calculate the destination point from given point having travelled the given distance (in km), on the given initial bearing (bearing may vary before destination is reached)
+        /// using the formula:
+        /// φ2 = asin( sin φ1 ⋅ cos δ + cos φ1 ⋅ sin δ ⋅ cos θ )
+        /// λ2 = λ1 + atan2(sin θ ⋅ sin δ ⋅ cos φ1, cos δ − sin φ1 ⋅ sin φ2)
+        /// where: φ is latitude, λ is longitude, θ is the bearing (clockwise from north), δ is the angular distance d/R; d being the distance travelled, R the earth’s radius
+        /// </summary>
+        /// <param name="point"></param>
+        /// <param name="bearing">Initial bearing in degrees from north.</param>
+        /// <param name="distance">Distance travelled, in same units as earth radius</param>
+        /// <returns>the destination point</returns>
+        /// <see cref="http://www.movable-type.co.uk/scripts/latlong.html"/>
         public static Location CalculateDestinationLocation(Location point, double bearing, double distance)
         {
 
@@ -49,7 +67,17 @@ namespace DroneSimulator.API.Helpers
             return new Location { latitude = RadianToDegree(lat2), longitude = RadianToDegree(lon2) };
         }
 
-        // Calculate the distance between two points in km
+        /// <summary>
+        /// Calculate the distance between two points in km
+        /// using the Haversine formula:
+        /// a = sin²(Δφ/2) + cos φ1 ⋅ cos φ2 ⋅ sin²(Δλ/2)
+        /// c = 2 ⋅ atan2( √a, √(1−a) )
+        /// d = R ⋅ c
+        /// where: φ is latitude, λ is longitude, R is earth’s radius(mean radius = 6,371km);
+        /// note that angles need to be in radians
+        /// </summary>
+        /// <returns>Distance between points "as the crow flies" in kilometers</returns>
+        /// <see cref="http://www.movable-type.co.uk/scripts/latlong.html"/>
         public static double CalculateDistanceBetweenLocations(Location startPoint, Location endPoint)
         {
 
